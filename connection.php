44da0fb160d08d3585069a7c217913b1ad2110d2
nafeelcassim <40332458+nafeelcassim@users.noreply.github.com>
2018-09-08 18:09:02 +0530

    xxxxx

--- connection.php
+++ connection.php
@@ -1,5 +1,14 @@
 <?php
 
+    $conn = new mysqli("localhost", "root", "");
 
+    mysqli_select_db($conn,"movie_hut");
+
+    // Check connection
+     if ($conn->connect_error) {
+           die("Connection failed: " . $conn->connect_error);
+      } 
+         
+     echo "Connected successfully";
 
 ?>
