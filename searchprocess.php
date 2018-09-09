25a41e32b8a31f18222e797b28df18ab4e50e3f0
nafeelcassim <40332458+nafeelcassim@users.noreply.github.com>
2018-09-09 17:29:39 +0530

    s

--- searchprocess.php
+++ searchprocess.php
@@ -1,21 +1,21 @@
 <?php
  
-     include(connection.php);
-     //name="search"
-     $searchbar = $_POST["search"];
      
-    //select process
-     $sql = "SELECT movieName FROM movie WHERE movieName = '$searchbar'";
-     $result = $conn->query($sql);
+      $value = $_POST["search"];
 
-    if ($result->num_rows > 0) {
-    // output data of each row
-         while($row = $result->fetch_assoc()) {
-        echo "successful";
-    }
-}     else {
-      echo "0 results";
-}
+      if(isset($value))
+      {
+      	 include(connection.php);
+
+      	  $sql = "SELECT movieName FROM movie WHERE moviename LIKE '$value'";
+      	  $query = mysqli_query($conn, $sql);
+
+      	  while($row = $query->fetch_array()){
+
+                   header("Location.usr.php");
+
+      	  }
+      }
 
 
 ?>
