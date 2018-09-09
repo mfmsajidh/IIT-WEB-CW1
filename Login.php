992a2aa90b0295adaabb57f3f5f926798aa539e8
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 12:19:35 +0530

    Change

--- Login.php
+++ Login.php
@@ -44,9 +44,9 @@
 					<h1 align="center" class="head1"> Log In </h1>
 
 						<form method="post" action="LoginProcess.php">
-						<input class ="text1" type="email" placeholder="Email" name="Email">
+						<input class ="text1" type="email" placeholder="Email" name="Email" required>
 						<br></br>
-						<input class ="text1" type="password" placeholder="Password" name="psw">
+						<input class ="text1" type="password" placeholder="Password" name="psw" required>
 						<br></br>
 						<button class="Sign1" type ="submit"> Sign In </button>
 												<br></br>
