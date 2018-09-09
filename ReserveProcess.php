0166bd0f07f1c0f426207d8d6b667f7236919117
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 18:35:20 +0530

    Busy

--- ReserveProcess.php
+++ ReserveProcess.php
@@ -2,6 +2,10 @@
 
 include("connection.php");
 
-$query = "INSERT INTO movie VALUES "
+$Customer_ID = $_POST[''];
+$Movie_ID = ;
+
+$query_1 = "INSERT INTO movie (customerId) VALUES ";
+$query_2 = "ALTER TABLE movie MODIFY COLUMN status "
 
 ?>
