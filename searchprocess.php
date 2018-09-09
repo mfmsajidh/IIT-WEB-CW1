ab5e3b6614903b90adc650fa5c84cb455844ba31
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 17:25:40 +0530

    Revert Search

--- searchprocess.php
+++ searchprocess.php
@@ -1,7 +1,6 @@
 <?php
  
- echo "Hello";
-/*     include(connection.php);
+    include(connection.php);
      //name="search"
      $searchbar = $_POST["search"];
      
@@ -17,7 +16,5 @@
 }     else {
       echo "0 results";
 }
-
-*/
 
 ?>
