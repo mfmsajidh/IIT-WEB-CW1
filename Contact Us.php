421b194733001224e9eeaac671850116e6368bc1
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 13:54:25 +0530

    Protected Pages Fix

--- Contact Us.php
+++ Contact Us.php
@@ -1,3 +1,19 @@
+<?php
+// You'd put this code at the top of any "protected" page you create
+
+// Always start this first
+session_start();
+
+if ( isset( $_SESSION['Email'] ) ) {
+    // Grab user data from the database using the user_id
+    // Let them access the "logged in only" pages
+} 
+else {
+    // Redirect them to the login page
+    header("Location: Contact Us - User.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
