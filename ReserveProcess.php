f0258ab19afbe0ce78049acc351a5e18e0210995
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 19:00:03 +0530

    Images Edit

--- ReserveProcess.php
+++ ReserveProcess.php
@@ -2,8 +2,10 @@
 
 include("connection.php");
 
-$Customer_ID = $_POST[''];
-$Movie_ID = ;
+$Customer_ID = $_POST[""];
+$Movie_ID = $_POST[''];.
+
+
 
 $query_1 = "INSERT INTO movie (customerId) VALUES ";
 $query_2 = "ALTER TABLE movie MODIFY COLUMN status "
