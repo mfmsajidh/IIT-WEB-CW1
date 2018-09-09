010851ff9b04eebfa6d71749b65d53334815fd8f
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 13:57:47 +0530

    Protection Fix

--- Register.php
+++ Register.php
@@ -7,10 +7,11 @@ session_start();
 if ( isset( $_SESSION['Email'] ) ) {
     // Grab user data from the database using the user_id
     // Let them access the "logged in only" pages
+  header("Location: User.php");
 } 
 else {
     // Redirect them to the login page
-    header("Location: User.php");
+    header("Location: Register.php");
 }
 ?>
 
