fcb7ba3f2efcb5804d400c6a4c53cd1f4f5b8f1a
nafeelcassim <40332458+nafeelcassim@users.noreply.github.com>
2018-09-10 04:17:51 +0530

    xxxx

--- SearchProcess.php
+++ SearchProcess.php
@@ -1,6 +1,20 @@
 <?php
 
+   session_start();
    include("connection.php");
    
-   
+   $searchbar = $_POST["search12"];
+
+
+   $result = mysqli_query($conn,"SELECT * FROM movie where movieName='$searchbar'") or die ("Could not execute query: ".mysqli_error($conn));
+
+
+   while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+   {
+     $_SESSION["search12"] = $row;
+      
+     
+      header("location:MovieViewer.php");
+   }
+
 ?>
