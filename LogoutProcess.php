3c0176ff4a5f16ba10b2ec6bea568673e8da0afa
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-09 14:16:26 +0530

    Minor Fix

--- LogoutProcess.php
+++ LogoutProcess.php
@@ -4,6 +4,18 @@ session_start();
 
 // Destroying the session clears the $_SESSION variable, thus "logging" the user
 // out. This also happens automatically when the browser is closed
+
+/*
+if(isset($_SESSION['Email'])){
+	session_destroy();
+	echo "<script>location.href='index.php'</script>";
+}
+else{
+	echo "<script>location.href='login.php'</script>";
+}
+*/
+
 session_destroy();
 header('location:index.php');
+
 ?>
