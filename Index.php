774ffa2618e110db23dab4fb169642a3b91dcaa4
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-10 07:29:43 +0530

    Minor Changes

--- Index.php
+++ Index.php
@@ -141,86 +141,62 @@ else {
 
 	<!-- Image Gallery -->
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Pacific Rim.jpg" alt="Pacific Rim" width="300" height="200">
-		  </a>
 		  <div class="desc"">Pacific Rim Uprising</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Justice League.jpg" alt="Justice League" width="300" height="200">
-		  </a>
 		  <div class="desc">Justice League</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Warcraft.jpg" alt="Warcraft" width="300" height="200">
-		  </a>
 		  <div class="desc">Warcraft: The Beginning</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Dawn of The Planet of The Apes.jpg" alt="Dawn of The Planet of The Apes" width="300" height="200">
-		  </a>
 		  <div class="desc">Dawn of The Planet of The Apes</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Skyfall 007.jpg" alt="Skyfall 007" width="300" height="200">
-		  </a>
 		  <div class="desc">Skyfall 007</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Rio 2.jpg" alt="Rio 2" width="300" height="200">
-		  </a>
 		  <div class="desc">Rio 2</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Assassins Creed.jpg" alt="Assassins Creed" width="300" height="200">
-		  </a>
 		  <div class="desc">Assassin's Creed</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Hobbit.jpg" alt="Hobbit" width="300" height="200">
-		  </a>
 		  <div class="desc">Hobbit: An Unexpected Journey</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Foreigner.jpg" alt="Foreigner" width="300" height="200">
-		  </a>
 		  <div class="desc">The Foreigner</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Happy New Year.jpg" alt="Happy New Year" width="300" height="200">
-		  </a>
 		  <div class="desc">Happy New Year</div>
 		</div>
 
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/Dracula.jpg" alt="Dracula" width="300" height="200">
-		  </a>
 		  <div class="desc">Dracula</div>
 		</div>
 		
 		<div class="gallery">
-		  <a target="_blank" href="#">
 		    <img src="Images/Background/MI6.jpg" alt="Mission Impossible Fallout" width="300" height="200">
-		  </a>
 		  <div class="desc">Mission Impossible 6: Fallout</div>
 		</div>
 
