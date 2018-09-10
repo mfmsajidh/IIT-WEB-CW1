2e67763461f8e0529586f14ca82a8235d55d768e
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-10 14:44:32 +0530

    Change

--- ReserveProcess.php
+++ ReserveProcess.php
@@ -4,10 +4,8 @@ session_start();
 
 include("connection.php");
 
-$id= $_GET['id'];
-
+$Movie_ID = $_GET['id'];
 $CustomerId = $_SESSION['CustomerId'];
-$Movie_ID = $id;
 
 $result = mysqli_query($conn, "UPDATE movie SET status='Borrowed', customerId='$CustomerId' WHERE movieId='$Movie_ID'") or die ("Could not execute query: ".mysqli_error($conn));
 
