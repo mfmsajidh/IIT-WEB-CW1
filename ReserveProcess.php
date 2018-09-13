889b684a3f68d2662a9834d49d8bfa2ec01f7dc0
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-13 15:11:45 +0530

    Minor Fix

--- ReserveProcess.php
+++ ReserveProcess.php
@@ -7,7 +7,7 @@ include("connection.php");
 $Movie_ID = $_GET['id'];
 $CustomerId = $_SESSION['CustomerId'];
 
-$result = mysqli_query($conn, "UPDATE movie SET status='Borrowed', customerId='$CustomerId' WHERE movieId='$Movie_ID'") or die ("Could not execute query: ".mysqli_error($conn));
+$result = mysqli_query($conn, "UPDATE movie SET status='Borrowed', customerId='$CustomerId', btn_status='Inform Me' WHERE movieId='$Movie_ID'") or die ("Could not execute query: ".mysqli_error($conn));
 
 header("location:Movie - Dynamic.php?id=$Movie_ID");
 
