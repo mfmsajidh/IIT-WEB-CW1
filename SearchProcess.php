a5a10fe0040c469944d2a65d7e97f2a5f24a92ae
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-13 13:06:53 +0530

    Minor Search FIxes

--- SearchProcess.php
+++ SearchProcess.php
@@ -9,7 +9,8 @@
 
    $result = mysqli_query($conn,"SELECT * FROM movie where movieName='$searchbar' or actor='$searchbar' or actress='$searchbar'") or die ("Could not execute query: ".mysqli_error($conn));
 
-    $row=mysqli_fetch_assoc($result);
+    
+  $row=mysqli_fetch_assoc($result);
 
     $movieId = $row["movieId"];
 
