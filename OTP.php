e053faf2496a947c8ba71db8ac014a9904aafed4
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2018-09-13 16:29:03 +0530

    fix

--- OTP.php
+++ OTP.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Fogot H U T?</title>
+	<title>Fogot H U T</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
